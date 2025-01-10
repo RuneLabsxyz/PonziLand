15a6e4f9723d27d19b713854dc5486959d73ac3b
hadouin <had@runelabs.xyz>
2025-01-10 16:11:33 +0100

    wip: getting token balances

--- client/src/lib/components/wallet/wallet-lookup.svelte
+++ client/src/lib/components/wallet/wallet-lookup.svelte
@@ -5,16 +5,24 @@
   import Button from '../ui/button/button.svelte';
   import Card from '../ui/card/card.svelte';
   import { useController } from '$lib/accounts/controller';
+  import { fetchBLUEBalance } from '$lib/accounts/balances';
 
   const { store, client: sdk, account } = useDojo();
   const controller = useController();
-  
-  const accountData = $derived(account.getAccount())
+
+  const accountData = $derived(account.getAccount());
 
   const handleShowInventory = () => {
-    controller.openProfile("inventory");
+    controller.openProfile('inventory');
   };
 
+  let balance = $state(0);
+
+  $effect(() => {
+    fetchBLUEBalance(accountData?.address ?? '').then((res) =>
+      console.log('from component BLUE:', res),
+    );
+  });
 </script>
 
 <div class="fixed top-0 right-0 z-50">
@@ -22,11 +30,13 @@
     <Card>
       <p>Wallet: {accountData?.address}</p>
       <pre>{accountData}</pre>
+      <Button onclick={() => handleShowInventory()}>inventory</Button>
+    </Card>
+
+    <Card>
+      <pre>{accountData}</pre>
     </Card>
   {:else}
     <Button class="m-2">Connect Wallet</Button>
   {/if}
-  <Button onclick={() => handleShowInventory()}>
-    inventory
-  </Button>
 </div>
