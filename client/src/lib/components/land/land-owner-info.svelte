63eab23b0c58ffcc1397f9d67a5c0de63cb9a754
Veikkan <pierre.semanne@gmail.com>
2025-04-23 20:09:21 +0200

    feat: address in pro mode + correct crown detection

---
+++ client/src/lib/components/land/land-owner-info.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Card } from '../ui/card';
+  import type { SelectedLand } from '$lib/stores/stores.svelte';
+
+  let {
+    land,
+  }: {
+    land?: SelectedLand;
+  } = $props();
+</script>
+
+<div class="absolute left-0 -translate-y-12">
+  <Card>
+    <div class="flex items-center gap-2 text-ponzi-number">
+      <span>{land?.owner?.slice(0, 4) + '...' + land?.owner?.slice(-4)}</span>
+    </div>
+  </Card>
+</div>
+
+<style>
+  .text-ponzi-number {
+    font-family: 'PonziNumber', sans-serif;
+  }
+</style>
