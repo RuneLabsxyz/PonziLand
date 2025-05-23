06d601798e2991a5b8fc8e8b9722678493ff32eb
hadouin <had@runelabs.xyz>
2025-05-23 18:14:41 +0200

    create land info tabs

---
+++ client/src/lib/components/+game-ui/widgets/land-info/info-tabs.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import OverallTab from './tabs/overall-tab.svelte';
+  import DailyTab from './tabs/daily-tab.svelte';
+  import HistoryTab from './tabs/history-tab.svelte';
+  import Button from '$lib/components/ui/button/button.svelte';
+
+  type TabType = 'overall' | 'daily' | 'history';
+  let activeTab: TabType = 'overall';
+
+  function setActiveTab(tab: TabType) {
+    activeTab = tab;
+  }
+</script>
+
+<div class="w-full h-full flex flex-col mt-6">
+  <div class="flex gap-2 w-full justify-center">
+    <Button 
+      class="w-full {activeTab === 'overall' ? '' : 'opacity-50'}" 
+      variant={activeTab === 'overall' ? "blue" : undefined} 
+      on:click={() => setActiveTab('overall')}
+    >
+      OVERALL
+    </Button>
+    <Button 
+      class="w-full {activeTab === 'daily' ? '' : 'opacity-50'}" 
+      variant={activeTab === 'daily' ? "blue" : undefined} 
+      on:click={() => setActiveTab('daily')}
+    >
+      DAILY
+    </Button>
+    <Button 
+      class="w-full {activeTab === 'history' ? '' : 'opacity-50'}" 
+      variant={activeTab === 'history' ? "blue" : undefined} 
+      on:click={() => setActiveTab('history')}
+    >
+      HISTORY
+    </Button>
+  </div>
+  
+  <div class="w-full h-full mt-4">
+    <OverallTab isActive={activeTab === 'overall'} />
+    <DailyTab isActive={activeTab === 'daily'} />
+    <HistoryTab isActive={activeTab === 'history'} />
+  </div>
+</div> 
---
+++ client/src/lib/components/+game-ui/widgets/land-info/land-infos.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { LandWithActions } from '$lib/api/land';
+  import LandOverview from '$lib/components/+game-map/land/land-overview.svelte';
+  import InfoTabs from './info-tabs.svelte';
+
+  let { land }: { land: LandWithActions } = $props();
+</script>
+
+<div class="h-full w-full flex flex-col">
+  <div class="w-full flex">
+    <div class="">
+      <LandOverview {land} size="lg" />
+    </div>
+    <InfoTabs />
+  </div>
+</div>
