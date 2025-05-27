18b5647a851b389f6c1c56a21f1283d538bdc55c
hadouin <had@runelabs.xyz>
2025-05-27 15:08:56 +0200

    wip: display only supported widgets

--- client/src/lib/components/+game-ui/widgets/widgets.config.ts
+++ client/src/lib/components/+game-ui/widgets/widgets.config.ts
@@ -10,6 +10,12 @@ export const availableWidgets: Widget[] = [
     id: 'my-lands',
     type: 'my-lands',
     label: 'My Lands',
-    icon: '/ui/icons/Icon_MyLand.png',
+    icon: '/ui/icons/Icon_Thin_MyLand.png',
   },
+  // {
+  //   id: 'notifications',
+  //   type: 'notifications',
+  //   label: 'Notifications',
+  //   icon: '/ui/icons/Icon_Thin_Notification.png',
+  // },
 ];
