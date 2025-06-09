895db96c7d0bec0acadc5805ed3ade913834892e
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2025-06-09 19:28:38 +0100

    fix formating

--- client/src/lib/monitoring/faro.ts
+++ client/src/lib/monitoring/faro.ts
@@ -1,28 +1,32 @@
 import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
-
 let faro = null;
 
-if (process.env.PUBLIC_DOJO_PROFILE !== 'dev' && process.env.PUBLIC_FARO_COLLECTOR_URL) {
-    faro = initializeFaro({
-        url: process.env.PUBLIC_FARO_COLLECTOR_URL,
-        app: {
-            name: 'Ponziland ',
-            version: 'ded7df3e539ef56fa717255ce3565a0aee66461c',
-            environment: process.env.PUBLIC_DOJO_PROFILE,
-        },
-        instrumentations: [...getWebInstrumentations(), new TracingInstrumentation()],
-    });
-
+if (
+  process.env.PUBLIC_DOJO_PROFILE !== 'dev' &&
+  process.env.PUBLIC_FARO_COLLECTOR_URL
+) {
+  faro = initializeFaro({
+    url: process.env.PUBLIC_FARO_COLLECTOR_URL,
+    app: {
+      name: 'Ponziland ',
+      version: 'ded7df3e539ef56fa717255ce3565a0aee66461c',
+      environment: process.env.PUBLIC_DOJO_PROFILE,
+    },
+    instrumentations: [
+      ...getWebInstrumentations(),
+      new TracingInstrumentation(),
+    ],
+  });
 }
 
 export const sendError = (error: Error, tags?: Record<string, any>) => {
-    if (faro) {
-        faro.api.pushError(error, { context: tags });
-    } else {
-        console.warn('Faro not initialized, error not sent');
-    }
+  if (faro) {
+    faro.api.pushError(error, { context: tags });
+  } else {
+    console.warn('Faro not initialized, error not sent');
+  }
 };
 
 export default faro;
