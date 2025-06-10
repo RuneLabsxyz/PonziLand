3c3f85c5b59026a7a4187b0d5f25564469fb6c24
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2025-06-10 10:32:49 +0100

    requested changes

--- client/src/lib/monitoring/faro.ts
+++ client/src/lib/monitoring/faro.ts
@@ -1,16 +1,16 @@
 import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 import { dev as isDev } from '$app/environment';
+import { PUBLIC_GIT_COMMIT_HASH } from '$env/static/public';
 
 let faro = null;
-const gitHash = process.env.GIT_COMMIT_HASH;
 
 if (!isDev && process.env.PUBLIC_FARO_COLLECTOR_URL) {
   faro = initializeFaro({
     url: process.env.PUBLIC_FARO_COLLECTOR_URL,
     app: {
       name: 'Ponziland ',
-      version: gitHash,
+      version: PUBLIC_GIT_COMMIT_HASH,
       environment: process.env.PUBLIC_DOJO_PROFILE,
     },
     instrumentations: [
