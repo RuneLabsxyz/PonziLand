8d3f8f409cb5486f3e848737ffdc8ede947f96a6
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2025-07-11 15:04:29 +0200

    fix build

--- client/src/lib/components/+game-map/land/land-nuke-time.svelte
+++ client/src/lib/components/+game-map/land/land-nuke-time.svelte
@@ -9,12 +9,19 @@
     land?: LandWithActions;
   } = $props();
 
-  let nukeTime: Promise<string | undefined> = $derived(calculateNukeTime());
+  let nukeTime = $state<string | undefined>(undefined);
 
-  async function calculateNukeTime(): Promise<string | undefined> {
-    if (land == undefined) return;
-    return parseNukeTime(await estimateNukeTime(land)).toString();
-  }
+  $effect(() => {
+    const calculateNukeTime = async () => {
+      if (land === undefined) {
+        nukeTime = undefined;
+        return;
+      }
+      const timeInSeconds = await estimateNukeTime(land);
+      nukeTime = parseNukeTime(timeInSeconds).toString();
+    };
+    calculateNukeTime();
+  });
 
   function formatNukeTime(nukeTime: string | undefined) {
     if (land?.getNeighbors().getNeighbors().length == 0) return 'inf';
@@ -24,7 +31,7 @@
 
 <div class="flex items-center gap-2 text-ponzi-number text-red-500">
   <img src="/extra/nuke.png" alt="Nuke Symbol" class="h-6 w-6" />
-  <span>{formatNukeTime(await nukeTime)}</span>
+  <span>{formatNukeTime(nukeTime)}</span>
 </div>
 
 <style>
