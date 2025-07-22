a5b212f3f9e113fa4515ea4c9e4de90bb01ff822
ZackAmes <zames6584@gmail.com>
2025-07-22 17:02:16 -0400

    use manifest from config

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -125,7 +125,7 @@ for (const val of Object.entries(envProfile)) {
 
 console.log(process.env['BYPASS_TOKEN']);
 
-const manifestPath = `../contracts/manifest_${profile}.json`;
+const manifestPath = profile == 'katana' ? `/etc/config/manifest_dev.json` : `../contracts/manifest_${profile}.json`;
 const dataPath = profile == 'katana' ? `/etc/config/katana.json` : `data/${profile}.json`;
 
 console.log('Manifest: ', manifestPath);
