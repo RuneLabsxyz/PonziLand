39edf8ce6b116aafc497c78305b06c781f2ce48e
ZackAmes <zames6584@gmail.com>
2025-07-25 13:03:41 -0400

    try fix build

--- client/src/lib/accounts/controller.ts
+++ client/src/lib/accounts/controller.ts
@@ -98,7 +98,7 @@ export async function setupController(
   }
 
   const controller = new SvelteController({
-    defaultChainId: "0x4b4154414e41", // SN_SEPOLIA in hex
+    defaultChainId: "0x57505f4b4154414e41", // SN_SEPOLIA in hex
     chains: [{ rpcUrl: config.rpcUrl }],
     policies: preset.chains.SN_MAIN.policies as any,
   });
--- client/svelte.config.js
+++ client/svelte.config.js
@@ -43,9 +43,9 @@ const profiles = {
     PUBLIC_GIT_COMMIT_HASH: null,
   },
   katana: {
-    PUBLIC_DOJO_RPC_URL: 'https://api.runelabs.xyz/katana',
+    PUBLIC_DOJO_RPC_URL: 'https://api.runelabs.xyz/x/katana/katana',
     PUBLIC_DOJO_TORII_URL: 'https://api.runelabs.xyz/torii',
-    PUBLIC_DOJO_CHAIN_ID: 'KATANA',
+    PUBLIC_DOJO_CHAIN_ID: 'WP_KATANA',
     PUBLIC_AVNU_URL: 'https://sepolia.api.avnu.fi',
     PUBLIC_EKUBO_URL: 'https://sepolia-api.ekubo.org',
     PUBLIC_DOJO_BURNER_ADDRESS: null,
