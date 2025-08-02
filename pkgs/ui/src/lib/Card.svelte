be6f655faf7bbb6a0c9bc1f3c32bf905fc9666b0
Mugen <pierre.semanne@gmail.com>
2025-08-02 14:24:51 +0200

    feat: added stuff

---
+++ pkgs/ui/src/lib/Card.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { HTMLAttributes, Snippet } from 'svelte';
+  import { clsx, type ClassValue } from 'clsx';
+  import './Card.css';
+
+  function cn(...inputs: ClassValue[]) {
+    return clsx(inputs);
+  }
+
+  interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    class?: string;
+    children?: Snippet;
+  }
+
+  let {
+    class: className,
+    children,
+    ...restProps
+  }: CardProps = $props();
+
+  const cardClass = cn(
+    'card',
+    className
+  );
+</script>
+
+<div
+  class={cardClass}
+  {...restProps}
+>
+  {@render children?.()}
+</div>
--- pkgs/ui/src/lib/index.ts
+++ pkgs/ui/src/lib/index.ts
@@ -1,5 +1,6 @@
 // Reexport your entry components here
 import Test from "./Test.svelte"
 import Button from "./Button.svelte"
+import Card from "./Card.svelte"
 
-export { Test, Button }
+export { Test, Button, Card }
