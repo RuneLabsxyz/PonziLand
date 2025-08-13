c554e517a450a6e5a9fc58afd1c2f3ea45a94ce9
ZackAmes <zames6584@gmail.com>
2025-08-13 12:35:30 -0400

    fix file name

--- playtest/scripts/commands/deploy_tokens.ts
+++ playtest/scripts/commands/deploy_tokens.ts
@@ -154,7 +154,7 @@ export async function  deployToken(config: Configuration, args: string[]) {
 
 async function addTokenToFile(config: Configuration, token: Token) {
   // Read the tokens file
-  const tokensPath = `${config.basePath}/depolyments/${config.deploymentName}/tokens.${config.environment}.json`;
+  const tokensPath = `${config.basePath}/depolyments/${config.deploymentName}/tokens.json`;
 
   // Check if file exists, create it if it doesn't
   try {
