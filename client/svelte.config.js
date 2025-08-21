a1a27072d0fb3636d72f84f23d7f21e38dfeabab
ZackAmes <zames6584@gmail.com>
2025-08-21 11:37:42 -0400

    update profile urls

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -97,7 +97,7 @@ const profiles = {
   deployment: {
     PUBLIC_DOJO_RPC_URL: 'https://api.cartridge.gg/x/starknet/sepolia',
     PUBLIC_DOJO_TORII_URL:
-      'https://play.ponzis.fun/torii',
+      'https://' + process.env['DEPLOY_NAME'] + '.ponzis.fun/torii',
     PUBLIC_DOJO_CHAIN_ID: 'SN_SEPOLIA',
     PUBLIC_AVNU_URL: 'https://sepolia.api.avnu.fi',
     PUBLIC_EKUBO_URL: 'https://sepolia-api.ekubo.org',
@@ -106,7 +106,7 @@ const profiles = {
     BYPASS_TOKEN: '',
     LAYERSWAP_TOKEN: '',
     PUBLIC_SOCIALINK_URL: 'https://socialink-sepolia.ponzi.land',
-    PUBLIC_PONZI_API_URL: 'https://play.ponzis.fun/api',
+    PUBLIC_PONZI_API_URL: 'https://' + process.env['DEPLOY_NAME'] + '.ponzis.fun/api',
     PUBLIC_FARO_COLLECTOR_URL:
       'https://faro-collector-prod-eu-west-2.grafana.net/collect/6b0946d2811fceca6349c46b402a3d2c',
     PUBLIC_GIT_COMMIT_HASH: process.env.PUBLIC_GIT_COMMIT_HASH,
