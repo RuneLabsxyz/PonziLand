3faff68698b78f19a5de9fd5f33e6b9350e8f3f0
ZackAmes <zames6584@gmail.com>
2025-08-22 15:35:06 -0400

    debug

--- client/src/lib/api/land_tiles.svelte.ts
+++ client/src/lib/api/land_tiles.svelte.ts
@@ -132,6 +132,7 @@ export class LandTileStore {
     const { initialEntities, subscription } = await setupLandsSubscription(
       client,
       (lands) => {
+        console.log('lands', lands);
         this.setEntities(lands);
       },
     );
