44f7ec07932e04ce56e270237380120574e5d897
hadouin <had@runelabs.xyz>
2025-08-26 15:00:15 +0200

    clouds react to mouse

--- client/src/lib/components/+game-map/three/cursor.store.svelte.ts
+++ client/src/lib/components/+game-map/three/cursor.store.svelte.ts
@@ -2,8 +2,10 @@ export let cursorStore: {
   hoveredTileIndex?: number;
   selectedTileIndex?: number;
   gridPosition?: { x: number; y: number; id: number };
+  absolutePosition?: { x: number; y: number };
 } = $state({
   hoveredTileIndex: undefined,
   selectedTileIndex: undefined,
   gridPosition: undefined,
+  absolutePosition: undefined,
 });
