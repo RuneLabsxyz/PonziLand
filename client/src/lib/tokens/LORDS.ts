f9606d2da2a652360513467e67e0e6673da301a6
hadouin <had@runelabs.xyz>
2025-09-25 16:18:05 +0200

    lords no reverse yet

--- client/src/lib/tokens/LORDS.ts
+++ client/src/lib/tokens/LORDS.ts
@@ -3,8 +3,59 @@ export const LORDS = {
   icon: '/tokens/LORDS/lords.png',
   biome: { x: 3, y: 1 },
   building: {
-    1: { x: 0, y: 0, useAnimation: false },
-    2: { x: 0, y: 1, useAnimation: false },
-    3: { x: 0, y: 2, useAnimation: false },
+    1: {
+      x: 0,
+      y: 0,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/LORDS/1-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [
+            { name: 'idle', frameRange: [0, 9] }
+          ],
+          bottomPadding: 15,
+          scale: 0.8,
+        },
+      ],
+    },
+    2: {
+      x: 0,
+      y: 1,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/LORDS/2-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [
+            { name: 'idle', frameRange: [0, 9] }
+          ],
+          bottomPadding: 10,
+          scale: 0.8,
+        },
+      ],
+    },
+    3: {
+      x: 0,
+      y: 2,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/LORDS/3-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [
+            { name: 'idle', frameRange: [0, 9] }
+          ],
+          bottomPadding: 10,
+          scale: 0.8,
+        },
+      ],
+    },
   },
 } as const;
