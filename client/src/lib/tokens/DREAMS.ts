7d4883e9dac29679cd2c1cc77f65f00aa95fa85a
hadouin <had@runelabs.xyz>
2025-09-25 16:03:03 +0200

    dreams

--- client/src/lib/tokens/DREAMS.ts
+++ client/src/lib/tokens/DREAMS.ts
@@ -3,8 +3,47 @@ export const DREAMS = {
   icon: '/tokens/DREAMS/dreams.png',
   biome: { x: 0, y: 5 },
   building: {
-    1: { x: 6, y: 9, useAnimation: false },
-    2: { x: 6, y: 10, useAnimation: false },
-    3: { x: 6, y: 11, useAnimation: false },
+    1: {
+      x: 6,
+      y: 9,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/DREAMS/1-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [{ name: 'idle', frameRange: [0, 9] }],
+        },
+      ],
+    },
+    2: {
+      x: 6,
+      y: 10,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/DREAMS/2-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [{ name: 'idle', frameRange: [0, 9] }],
+        },
+      ],
+    },
+    3: {
+      x: 6,
+      y: 11,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/DREAMS/3-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 4,
+          animations: [{ name: 'idle', frameRange: [0, 9] }],
+        },
+      ],
+    },
   },
 } as const;
