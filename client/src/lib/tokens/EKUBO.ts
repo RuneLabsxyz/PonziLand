03b2c79e92b3247d42077d3f3f687963a68dfa75
hadouin <had@runelabs.xyz>
2025-09-25 16:08:50 +0200

    ekubo

--- client/src/lib/tokens/EKUBO.ts
+++ client/src/lib/tokens/EKUBO.ts
@@ -1,10 +1,55 @@
+import { scale } from "svelte/transition";
+
 export const EKUBO = {
   skin: 'EKUBO',
   icon: '/tokens/EKUBO/ekubo.png',
   biome: { x: 0, y: 1 },
   building: {
-    1: { x: 0, y: 9, useAnimation: false },
-    2: { x: 0, y: 10, useAnimation: false },
-    3: { x: 0, y: 11, useAnimation: false },
+    1: {
+      x: 0,
+      y: 9,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/EKUBO/1-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          scale: 0.8,
+        },
+      ],
+    },
+    2: {
+      x: 0,
+      y: 10,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/EKUBO/2-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          bottomPadding: 10,
+          scale: 0.8,
+        },
+      ],
+    },
+    3: {
+      x: 0,
+      y: 11,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/EKUBO/3-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          bottomPadding: 6,
+        },
+      ],
+    },
   },
 } as const;
