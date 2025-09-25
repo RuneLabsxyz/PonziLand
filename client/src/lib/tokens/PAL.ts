4b48fd3905b7cb91c0052a1dad8ae3a5509cc4a3
hadouin <had@runelabs.xyz>
2025-09-25 16:22:02 +0200

    pal animation

--- client/src/lib/tokens/PAL.ts
+++ client/src/lib/tokens/PAL.ts
@@ -3,8 +3,53 @@ export const PAL = {
   icon: '/tokens/PAL/pal.png',
   biome: { x: 1, y: 2 },
   building: {
-    1: { x: 0, y: 3, useAnimation: false },
-    2: { x: 0, y: 4, useAnimation: false },
-    3: { x: 0, y: 5, useAnimation: false },
+    1: {
+      x: 0,
+      y: 3,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/PAL/1-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          bottomPadding: 15,
+          scale: 0.8,
+        },
+      ],
+    },
+    2: {
+      x: 0,
+      y: 4,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/PAL/2-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          bottomPadding: 10,
+          scale: 0.8,
+        },
+      ],
+    },
+    3: {
+      x: 0,
+      y: 5,
+      useAnimation: true,
+      animations: [
+        {
+          url: '/tokens/PAL/3-animated.png',
+          type: 'rowColumn',
+          width: 3,
+          height: 3,
+          animations: [{ name: 'idle', frameRange: [0, 6] }],
+          bottomPadding: 10,
+          scale: 0.8,
+        },
+      ],
+    },
   },
 } as const;
