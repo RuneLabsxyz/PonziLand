f53a462c40fb1a4f8ec1f54fa5edba4337f9d680
hadouin <hadouinleroy@gmail.com>
2025-10-06 13:06:10 +0200

    fix: only one land-info widget

--- client/src/lib/components/+game-ui/game-ui.svelte
+++ client/src/lib/components/+game-ui/game-ui.svelte
@@ -2,28 +2,14 @@
   import type { BaseLand, LandWithActions } from '$lib/api/land';
   import WidgetLauncher from '$lib/components/+game-ui/widgets/widget-launcher.svelte';
   import { widgetsStore } from '$lib/stores/widgets.store';
-  import { hexStringToNumber, locationIntToString } from '$lib/utils';
   import TxNotificationZone from '../ui/tx-notification-zone.svelte';
   import OverlayManager from './overlay-manager/OverlayManager.svelte';
   import WidgetProvider from './widgets/widget-provider.svelte';
 
-  // Function to open buy land widget
-  export function openBuyLandWidget(land: BaseLand) {
-    widgetsStore.addWidget({
-      id: `buy-land-${land.location.x}-${land.location.y}`,
-      type: 'buy-land',
-      position: { x: 300, y: 100 },
-      dimensions: { width: 200, height: 50 },
-      isMinimized: false,
-      isOpen: true,
-      data: { location: land.location },
-    });
-  }
-
   // Function to open land info widget
   export function openLandInfoWidget(land: LandWithActions) {
     widgetsStore.addWidget({
-      id: `land-info ${locationIntToString(land.location)} #${hexStringToNumber(land.location)}`,
+      id: 'land-info',
       type: 'land-info',
       position: { x: 100, y: 100 },
       dimensions: { width: 800, height: 0 },
