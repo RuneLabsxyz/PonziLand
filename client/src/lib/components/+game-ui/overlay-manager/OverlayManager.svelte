ce4c1ac2af3ec90ca34b4ec9e3c268bce4a91584
hadouin <hadouinleroy@gmail.com>
2025-10-08 16:36:49 +0200

    card layout for overlay

--- client/src/lib/components/+game-ui/overlay-manager/OverlayManager.svelte
+++ client/src/lib/components/+game-ui/overlay-manager/OverlayManager.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { devsettings } from '$lib/components/+game-map/three/utils/devsettings.store.svelte.js';
-  import * as ToggleGroup from '$lib/components/ui/toggle-group';
   import { HeatmapParameter } from '$lib/components/+game-ui/widgets/heatmap/heatmap.config';
+  import Card from '$lib/components/ui/card/card.svelte';
+  import { Separator } from '$lib/components/ui/separator';
+  import * as ToggleGroup from '$lib/components/ui/toggle-group';
   import { heatmapStore } from '$lib/stores/heatmap.svelte';
-  import { Toggle } from '$lib/components/ui/toggle';
   import { onMount } from 'svelte';
 
   type MultipleValues = ('nuke' | 'rates')[];
@@ -38,57 +39,65 @@
 </script>
 
 <div
-  class="top-0 left-1/2 -translate-x-1/2 absolute z-2 p-2"
+  class="top-0 left-1/2 -translate-x-1/2 absolute z-2"
   style="pointer-events: all;"
 >
-  <div class="flex gap-2">
-    <!-- <Toggle variant="map" size="sm" class="rounded">
+  <Card class="bg-ponzi">
+    <div class="flex gap-2 -m-2">
+      <!-- <Toggle variant="map" size="sm" class="rounded">
       <img
         src="/ui/icons/Icon_Book.png"
         alt="Settings"
         class="inline h-4 w-4"
       />
     </Toggle> -->
-    <ToggleGroup.Root
-      class="overflow-hidden"
-      size="sm"
-      variant="map"
-      type="single"
-      value={heatmapState}
-      onValueChange={(e) => (heatmapState = e as HeatmapState)}
-    >
-      <ToggleGroup.Item value={HeatmapParameter.SELL_PRICE}>
-        <img
-          src="/ui/icons/IconTiny_Stats.png"
-          alt="Stats Icon"
-          class="inline h-4 w-4"
-        />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value={HeatmapParameter.LEVEL}>
-        <img src="/ui/star/on.png" alt="Level" class="inline h-4 w-4" />
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
-    <ToggleGroup.Root
-      size="sm"
-      variant="map"
-      type="multiple"
-      value={multiple}
-      onValueChange={(e) => (multiple = e as MultipleValues)}
-    >
-      <ToggleGroup.Item value="nuke">
-        <img
-          src="/ui/icons/Icon_ShieldRed.png"
-          alt="Stats Icon"
-          class="inline h-4 w-4"
-        />
-      </ToggleGroup.Item>
-      <ToggleGroup.Item value="rates">
-        <img
-          src="/ui/icons/Icon_Coin3.png"
-          alt="Stats Icon"
-          class="inline h-4 w-4"
-        />
-      </ToggleGroup.Item>
-    </ToggleGroup.Root>
-  </div>
+      <ToggleGroup.Root
+        class="rounded-none"
+        size="sm"
+        variant="map"
+        type="single"
+        value={heatmapState}
+        onValueChange={(e) => (heatmapState = e as HeatmapState)}
+      >
+        <ToggleGroup.Item value={HeatmapParameter.SELL_PRICE}>
+          <div
+            class="flex items-center justify-center h-4 w-4 font-ponzi-number text-xs stroke-3d-black"
+          >
+            $
+          </div>
+        </ToggleGroup.Item>
+        <ToggleGroup.Item value={HeatmapParameter.LEVEL}>
+          <div
+            class="flex items-center justify-center h-4 w-4 font-ponzi-number text-xs stroke-3d-black"
+          >
+            lvl
+          </div>
+        </ToggleGroup.Item>
+      </ToggleGroup.Root>
+      <Separator orientation="vertical" class="my-2 opacity-50" />
+      <ToggleGroup.Root
+        size="sm"
+        variant="map"
+        class="rounded-none"
+        type="multiple"
+        value={multiple}
+        onValueChange={(e) => (multiple = e as MultipleValues)}
+      >
+        <ToggleGroup.Item value="nuke">
+          <img
+            src="/ui/icons/Icon_ShieldRed.png"
+            alt="Stats Icon"
+            class="inline h-5 w-5"
+          />
+        </ToggleGroup.Item>
+        <ToggleGroup.Item value="rates">
+          <img
+            src="/ui/icons/Icon_Coin3.png"
+            alt="Stats Icon"
+            class="inline h-5 w-5"
+          />
+        </ToggleGroup.Item>
+      </ToggleGroup.Root>
+    </div>
+  </Card>
 </div>
--- client/src/lib/components/ui/toggle/toggle.svelte
+++ client/src/lib/components/ui/toggle/toggle.svelte
@@ -2,13 +2,13 @@
   import { type VariantProps, tv } from 'tailwind-variants';
 
   export const toggleVariants = tv({
-    base: 'ring-offset-background hover:opacity-100 opacity-75 focus-visible:ring-ring data-[state=on]:bg-[#FFF2] data-[state=on]:opacity-100 inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+    base: 'ring-offset-background hover:opacity-100 opacity-80 focus-visible:ring-ring data-[state=on]:bg-[#FFF2] data-[state=on]:opacity-100 inline-flex items-center justify-center gap-2 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
     variants: {
       variant: {
         default: 'bg-transparent',
         outline:
           'border-input hover:bg-accent hover:text-accent-foreground border bg-transparent',
-        map: 'border border-[#FFF1] bg-[#0008]',
+        map: 'rounded-none',
       },
       size: {
         default: 'h-10 min-w-10 px-3',
