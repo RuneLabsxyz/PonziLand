5ae8e5272b213a9bde341a73ba2463ef39aad8e0
hadouin <hadchaos@gmail.com>
2025-10-08 11:37:47 +0000

    add overlaymanager to toggle nuke times (#1121)
    add overlaymanager to toggle nuke times
    
    add menubar from shadcn
    
    add toggles to overlay manager
    
    overaly changes for heatmap
    
    wip overlaymanager menubar
    
    wip: creating overlay-manager-controls

---
+++ client/src/lib/components/+game-ui/overlay-manager/OverlayManager.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import { devsettings } from '$lib/components/+game-map/three/utils/devsettings.store.svelte.js';
+  import * as ToggleGroup from '$lib/components/ui/toggle-group';
+  import { HeatmapParameter } from '$lib/components/+game-ui/widgets/heatmap/heatmap.config';
+  import { heatmapStore } from '$lib/stores/heatmap.svelte';
+  import { Toggle } from '$lib/components/ui/toggle';
+
+  type MultipleValues = ('nuke' | 'rates')[];
+
+  let multiple: MultipleValues = $state([]);
+
+  $effect(() => {
+    devsettings.showNukeTimes = multiple.includes('nuke');
+    devsettings.showLandOverlay = multiple.includes('rates');
+  });
+
+  type HeatmapState =
+    | HeatmapParameter.SELL_PRICE
+    | HeatmapParameter.STAKE_AMOUNT
+    | HeatmapParameter.LEVEL
+    | undefined;
+  let heatmapState = $state<HeatmapState>();
+
+  $effect(() => {
+    if (heatmapState) {
+      heatmapStore.setEnabled(true);
+      heatmapStore.setParameter(heatmapState);
+    } else {
+      heatmapStore.setEnabled(false);
+    }
+  });
+</script>
+
+<div
+  class="top-0 left-1/2 -translate-x-1/2 absolute z-2 p-2"
+  style="pointer-events: all;"
+>
+  <div class="flex gap-2">
+    <!-- <Toggle variant="map" size="sm" class="rounded">
+      <img
+        src="/ui/icons/Icon_Book.png"
+        alt="Settings"
+        class="inline h-4 w-4"
+      />
+    </Toggle> -->
+    <ToggleGroup.Root
+      class="overflow-hidden"
+      size="sm"
+      variant="map"
+      type="single"
+      value={heatmapState}
+      onValueChange={(e) => (heatmapState = e as HeatmapState)}
+    >
+      <ToggleGroup.Item value={HeatmapParameter.SELL_PRICE}>
+        <img
+          src="/ui/icons/IconTiny_Stats.png"
+          alt="Stats Icon"
+          class="inline h-4 w-4"
+        />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={HeatmapParameter.LEVEL}>
+        <img src="/ui/star/on.png" alt="Level" class="inline h-4 w-4" />
+      </ToggleGroup.Item>
+    </ToggleGroup.Root>
+    <ToggleGroup.Root
+      size="sm"
+      variant="map"
+      type="multiple"
+      value={multiple}
+      onValueChange={(e) => (multiple = e as MultipleValues)}
+    >
+      <ToggleGroup.Item value="nuke">
+        <img
+          src="/ui/icons/Icon_ShieldRed.png"
+          alt="Stats Icon"
+          class="inline h-4 w-4"
+        />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value="rates">
+        <img
+          src="/ui/icons/Icon_Coin3.png"
+          alt="Stats Icon"
+          class="inline h-4 w-4"
+        />
+      </ToggleGroup.Item>
+    </ToggleGroup.Root>
+  </div>
+</div>
