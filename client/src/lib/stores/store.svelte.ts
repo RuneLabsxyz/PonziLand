f60d4e134d0c2c078894816557a7c81741ae4b83
ZackAmes <zames6584@gmail.com>
2025-10-09 12:45:37 -0400

    move quest calls and fix imports

--- client/src/lib/stores/store.svelte.ts
+++ client/src/lib/stores/store.svelte.ts
@@ -9,7 +9,6 @@ import { useDojo } from '$lib/contexts/dojo';
 import { notificationQueue } from '$lib/stores/event.store.svelte';
 import { parseDojoCall } from '@dojoengine/core';
 import { getManifest } from '$lib/manifest';
-import { baseToken, tokenStore } from '$lib/stores/tokens.store.svelte';
 
 // Main stores following Dojo subscription pattern
 export let landStore = $state(new LandTileStore());
@@ -98,111 +97,6 @@ export async function bidLand(location: string, setup: LandSetup) {
       'Game client not ready. Please wait for initialization to complete.',
     );
   }
-}
-
-export async function SetLandQuest(location: string) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-
-  let res = await sdk.client.quests.setLandQuest(
-    account()?.getWalletAccount()!,
-    location,
-    2,
-  );
-  notificationQueue.addNotification(res?.transaction_hash ?? null, 'set quest tile');
-  return res;
-}
-
-export async function RemoveLandQuest(location: string) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-
-  let res = await sdk.client.quests.removeLandQuest(
-    account()?.getWalletAccount()!,
-    location,
-  );
-  notificationQueue.addNotification(res?.transaction_hash ?? null, 'remove quest tile');
-  return res;
-}
-
-export async function StartQuest(location: string, entry_price:number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.startQuest(
-    account()?.getWalletAccount()!,
-    location,
-    0,
-    entry_price,
-    baseToken?.address,
-  );
-  notificationQueue.addNotification(res?.transaction_hash ?? null, 'start quest');
-  return res;
-}
-
-export async function GetQuest(questId: number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.getQuest(
-    account()?.getWalletAccount()!,
-    questId,
-  );
-  return res;
-}
-
-export async function GetQuestToken(questId: number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.getQuestGameToken(
-    questId,
-  );
-  return res;
-}
-
-export async function GetQuestScore(questId: number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.getScore(
-    questId,
-  );
-  return res;
-}
-
-export async function GetQuestEntryPrice(questId: number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.getQuestEntryPrice(questId);
-  return res;
-}
-
-export async function ClaimQuest(questId: number) {
-  const { client: sdk, accountManager } = useDojo();
-  const account = () => {
-    return accountManager!.getProvider();
-  };
-  let res = await sdk.client.quests.claimLand(
-    account()?.getWalletAccount()!,
-    questId, 
-    '0x0',
-    0,
-    0,
-    0,
-    0
-  );
-  return res;
 }
 
 
