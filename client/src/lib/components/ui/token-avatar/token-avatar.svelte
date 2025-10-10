91dd5d08e68f4dfca0b55056004e8fe8cbe64208
hadouin <hadouinleroy@gmail.com>
2025-10-10 13:40:32 +0200

    perf: token avatar image

--- client/src/lib/components/ui/token-avatar/token-avatar.svelte
+++ client/src/lib/components/ui/token-avatar/token-avatar.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-  import * as Avatar from '$lib/components/ui/avatar/index.js';
   import type { Token } from '$lib/interfaces';
-  import { getTokenMetadata } from '$lib/utils';
+  import { cn, getTokenMetadata } from '$lib/utils';
 
   let { token = $bindable<Token>(), class: className = 'h-4 w-4' } = $props();
+  const metadata = $derived(getTokenMetadata(token.skin));
 </script>
 
-<Avatar.Root class={className}>
-  {@const metadata = getTokenMetadata(token.skin)}
-  <Avatar.Image
+<div class={cn('border-none',className)}>
+  <img
     src={metadata?.icon || '/tokens/default/icon.png'}
     alt={token.symbol}
     class="pointer-events-none select-none"
   />
-  <Avatar.Fallback>{token.symbol}</Avatar.Fallback>
-</Avatar.Root>
+</div>
