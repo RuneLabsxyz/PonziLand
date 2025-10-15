5ef08b3a860407f3d78cd05384732472c3c04b5b
Valentin Ricard <me@valentin.red>
2025-10-15 22:54:00 +0200

    feat: Added mobile block window

---
+++ client/src/lib/components/MobileBlock.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Card } from '$lib/components/ui/card';
+</script>
+
+<div
+  class="z-[9999] flex items-center justify-center bg-[#322637] px-4 w-screen h-screen"
+>
+  <Card class="flex flex-col bg-ponzi p-8 max-w-md">
+    <div class="text-center">
+      <h2 class="mb-4 text-2xl font-bold">Desktop Only</h2>
+      <p class="text-lg opacity-90">
+        Phones are not ponzi friendly, try desktop for a full experience.
+      </p>
+    </div>
+  </Card>
+</div>
