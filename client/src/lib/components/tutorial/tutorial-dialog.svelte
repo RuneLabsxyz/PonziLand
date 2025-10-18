9c7c0ffe7c3c2208f22ab1557688e98082be6c90
Mugen <pierre.semanne@gmail.com>
2025-10-18 16:24:05 +0200

    readd the tutorial modal

---
+++ client/src/lib/components/tutorial/tutorial-dialog.svelte
@@ -0,0 +1,21 @@
+<script lang=ts>
+    import { Card } from '$lib/components/ui/card';
+    import { tutorialState } from './stores.svelte';
+    import dialogData from './dialog.json';
+
+    let currentDialog = $derived(dialogData[tutorialState.tutorialStep - 1]);
+
+</script>
+
+<div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
+<Card>
+    <div class="flex flex-col gap-2 w-[400px] p-4">
+        <h1 class="text-2xl font-bold">Tutorial</h1>
+        {#if currentDialog}
+            <div class="text-sm">
+                {currentDialog.text}
+            </div>
+        {/if}
+    </div>
+</Card>
+</div>
