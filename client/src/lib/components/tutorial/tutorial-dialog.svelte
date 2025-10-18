4c3d3e7fbbbe4341388002c63b1d01f8797e7bcf
Mugen <pierre.semanne@gmail.com>
2025-10-18 16:26:28 +0200

    fix font

--- client/src/lib/components/tutorial/tutorial-dialog.svelte
+++ client/src/lib/components/tutorial/tutorial-dialog.svelte
@@ -9,8 +9,8 @@
 
 <div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
 <Card>
-    <div class="flex flex-col gap-2 w-[400px] p-4">
-        <h1 class="text-2xl font-bold">Tutorial</h1>
+    <div class="flex flex-col gap-2 w-[400px] p-4 font-ponzi-number">
+        <h1 class="text-2xl">Tutorial</h1>
         {#if currentDialog}
             <div class="text-sm">
                 {@html currentDialog.text}
