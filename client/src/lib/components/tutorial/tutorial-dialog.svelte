73390f4f8024d030cbb6ac6eb7f46311d65b746d
Mugen <pierre.semanne@gmail.com>
2025-10-18 16:49:53 +0200

    first tutorial steps

--- client/src/lib/components/tutorial/tutorial-dialog.svelte
+++ client/src/lib/components/tutorial/tutorial-dialog.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
-  import { Card } from '$lib/components/ui/card';
-  import { tutorialState } from './stores.svelte';
-  import dialogData from './dialog.json';
+    import { Card } from '$lib/components/ui/card';
+    import { tutorialState } from './stores.svelte';
+    import dialogData from './dialog.json';
+    import { onMount } from 'svelte';
+  
+    let currentDialog = $derived(dialogData[tutorialState.tutorialStep - 1]);
 
-  let currentDialog = $derived(dialogData[tutorialState.tutorialStep - 1]);
-</script>
-
-<div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
-  <Card>
-    <div class="flex flex-col gap-2 w-[400px] p-4 font-ponzi-number">
-      <h1 class="text-2xl">Tutorial</h1>
-      {#if currentDialog}
-        <div class="text-sm">
-          {@html currentDialog.text}
-        </div>
-      {/if}
-    </div>
-  </Card>
-</div>
+    onMount(() => {
+        tutorialState.tutorialStep = 1;
+        setTimeout(() => {
+            tutorialState.tutorialStep += 1;
+        }, 5000);
+    });
+  </script>
+  
+  <div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
+    <Card>
+      <div class="flex items-center gap-2 w-[400px] h-[200px] p-4 font-ponzi-number">
+        {#if currentDialog}
+        <div class="w-32 flex-shrink-0">
+          <img
+              src={`/tutorial/ponziworker_${currentDialog.image_id}.png`}
+              alt="Ponzi Worker"
+              class="h-full w-full object-contain"
+              />
+          </div>
+          <div class="text-sm">
+            {@html currentDialog.text}
+          </div>
+        {/if}
+      </div>
+    </Card>
+  </div>
