8feb4f0b9f88988d2e3ea462684b1b64e1a02f02
Mugen <pierre.semanne@gmail.com>
2025-10-21 00:45:45 +0200

    chore: prettier

--- client/src/lib/components/tutorial/tutorial-dialog.svelte
+++ client/src/lib/components/tutorial/tutorial-dialog.svelte
@@ -1,21 +1,20 @@
-<script lang=ts>
-    import { Card } from '$lib/components/ui/card';
-    import { tutorialState } from './stores.svelte';
-    import dialogData from './dialog.json';
+<script lang="ts">
+  import { Card } from '$lib/components/ui/card';
+  import { tutorialState } from './stores.svelte';
+  import dialogData from './dialog.json';
 
-    let currentDialog = $derived(dialogData[tutorialState.tutorialStep - 1]);
-
+  let currentDialog = $derived(dialogData[tutorialState.tutorialStep - 1]);
 </script>
 
 <div class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
-<Card>
+  <Card>
     <div class="flex flex-col gap-2 w-[400px] p-4 font-ponzi-number">
-        <h1 class="text-2xl">Tutorial</h1>
-        {#if currentDialog}
-            <div class="text-sm">
-                {@html currentDialog.text}
-            </div>
-        {/if}
+      <h1 class="text-2xl">Tutorial</h1>
+      {#if currentDialog}
+        <div class="text-sm">
+          {@html currentDialog.text}
+        </div>
+      {/if}
     </div>
-</Card>
+  </Card>
 </div>
