17fb2b5b4859b94efff33495c71e76dbe40b9552
hadouin <hadouinleroy@gmail.com>
2025-11-09 20:55:38 +0100

    template image fast

---
+++ client/src/routes/pnl/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import PnlImage from '$lib/components/+game-ui/widgets/command-center/PnlImage.svelte';
+  import { page } from '$app/state';
+
+  const value = $derived(Number(page.url.searchParams.get('value')) || 0);
+</script>
+
+<PnlImage {value} />
