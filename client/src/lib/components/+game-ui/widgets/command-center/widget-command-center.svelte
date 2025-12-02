f589dcb32fb4dcfba624c82fa54a023917f573dc
Hadouin <79368202+hadouin@users.noreply.github.com>
2025-12-02 16:57:35 +0100

    Update widget-command-center.svelte

--- client/src/lib/components/+game-ui/widgets/command-center/widget-command-center.svelte
+++ client/src/lib/components/+game-ui/widgets/command-center/widget-command-center.svelte
@@ -140,7 +140,7 @@
         class={[
           'h-6 w-6',
           {
-            'drop-shadow-[0_0_2px_rgba(255,255,0,0.8)]': activeTab === 'market',
+            'drop-shadow-[0_0_2px_rgba(255,255,0,0.8)]': activeTab === 'history',
           },
         ]}
       />
