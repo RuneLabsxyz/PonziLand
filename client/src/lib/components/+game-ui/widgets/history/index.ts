19e1f83fab1740da24f67f35af9bf00f9660f094
hadouin <hadchaos@gmail.com>
2025-12-09 10:14:33 +0000

    history ui (#1290)

--- client/src/lib/components/+game-ui/widgets/history/index.ts
+++ client/src/lib/components/+game-ui/widgets/history/index.ts
@@ -1,6 +1,4 @@
 export { default as WidgetHistory } from './widget-history.svelte';
 export { default as HistoryList } from './history-list.svelte';
 export { default as HistoryEntry } from './history-entry.svelte';
-export { default as LandBoughtEntry } from './land-bought-entry.svelte';
-export { default as LandNukedEntry } from './land-nuked-entry.svelte';
 export { formatTimestamp } from './utils';
