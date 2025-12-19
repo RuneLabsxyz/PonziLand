5f709b57a41bb2874ff5bbe33263a54ee97eb224
Mugen <pierre.semanne@gmail.com>
2025-12-19 14:39:00 +0100

    chore: prettier

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -142,7 +142,8 @@ const profiles = {
     LAYERSWAP_TOKEN: '',
     PAYMASTER_API_KEY: '',
     PUBLIC_SOCIALINK_URL: 'https://socialink.ponzi.land',
-    PUBLIC_PONZI_API_URL: 'https://api.runelabs.xyz/ponziland-mainnet-temp/api/',
+    PUBLIC_PONZI_API_URL:
+      'https://api.runelabs.xyz/ponziland-mainnet-temp/api/',
     PUBLIC_BRIDGE_API_URL: 'https://ponzi.land',
     PUBLIC_FARO_COLLECTOR_URL:
       'https://faro-collector-prod-eu-west-2.grafana.net/collect/6b0946d2811fceca6349c46b402a3d2c',
